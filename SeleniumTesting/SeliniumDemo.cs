60f93a8aed851b229d43de7a2c06f160010b2cfa
Peter Thompson <pnt510@gmail.com>
2018-03-16 22:02:56 -0400

    Selenium testing

---
+++ SeleniumTesting/SeliniumDemo.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTesting
+{
+    class SeliniumDemo
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:\\Users\\Peter\\Documents\\GitHub\\SeleniumTesting\\SeleniumTesting\\bin");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+            IWebElement element = driver.FindElement(By.XPath("//*[@id=\"lst-ib\"]"));
+            element.SendKeys("Hello World!");
+            element = driver.FindElement(By.XPath("//*[@id=\"tsf\"]/div[2]/div[3]/center/input[1]"));
+            element.Click();
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
