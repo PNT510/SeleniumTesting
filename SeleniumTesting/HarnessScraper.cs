8359305dc78949ce7b9ea3ce5577cb345680b571
Peter Thompson <pnt510@gmail.com>
2018-04-23 19:54:07 -0400

    scrape harness site for ride data

---
+++ SeleniumTesting/HarnessScraper.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTesting
+{
+    class HarnessScraper
+    {
+        class SeliniumDemo
+        {
+            IWebDriver driver;
+            string filePath = @"C:\Users\Peter\Documents\GitHub\SeleniumTesting\SeleniumTesting\files\file.txt";
+            [SetUp]
+            public void startBrowser()
+            {
+                driver = new ChromeDriver(@"C:\Users\Peter\Documents\GitHub\SeleniumTesting\SeleniumTesting\files");
+                
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+            }
+
+            [Test]
+            public void Harness()
+            {
+                harnessLogin();
+
+                while(elementExists("next"))
+                {
+                    List<string> lines = new List<string>();
+                    string line = "";                    
+                    for(int row = 1; row <= 20; row++)
+                    {
+                        for (int column = 2; column <= 7; column++)
+                        {
+                            string xPath = "//*[@id=\"classlisting\"]/tbody/tr["+row+"]/td["+column+"]";
+                            if (elementExistsXPath(xPath))
+                            {
+                                line += driver.FindElement(By.XPath(xPath)).Text;
+                            }
+                            line += ",";
+                        }
+                        lines.Add(line);
+                        line = "";
+                    }
+
+                    using (StreamWriter sw = new StreamWriter(filePath, true))
+                    {
+                        foreach(string s in lines)
+                        {
+                            sw.WriteLine(s);
+                        }                        
+                    }
+                    if (elementExists("next"))
+                    {
+                        driver.FindElement(By.ClassName("next")).Click();
+                    }                        
+                }    
+                
+                
+            }
+
+            [TearDown]
+            public void closeBrowser()
+            {
+                driver.Close();
+            }
+
+
+            private void harnessLogin()
+            {
+                string userName = "";
+                string password = "";
+                driver.Url = "https://www.harnesscycle.com/reserve/index.cfm?action=Account.classes";
+                driver.FindElement(By.XPath("//*[@id=\"supernav\"]/a[1]")).Click(); //Top Nav Login
+                driver.FindElement(By.XPath("//*[@id=\"username\"]")).SendKeys(userName);
+                driver.FindElement(By.XPath("//*[@id=\"password\"]")).SendKeys(password);
+                driver.FindElement(By.XPath("//*[@id=\"signinbox\"]/form/div[4]/button")).Click(); //Sign in
+            }
+
+            private Boolean elementExists(string className)
+            {
+                try
+                {
+                    driver.FindElement(By.ClassName(className));
+                } catch(NoSuchElementException e)
+                {
+                    return false;
+                }
+                return true;
+            }
+
+            private Boolean elementExistsXPath(string xPath)
+            {
+                try
+                {
+                    driver.FindElement(By.XPath(xPath));
+                }
+                catch (NoSuchElementException e)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
